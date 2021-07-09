a8e47b862f3acff707e1c0c63c35a31b3185b14e
Juan Daniel <juan.perez@wolox.co>
2021-07-09 18:11:48 -0500

    Creation of the Get, Post and Delete methods within book controller

--- training/src/main/java/wolox/training/controllers/BookController.java
+++ training/src/main/java/wolox/training/controllers/BookController.java
@@ -1,16 +1,50 @@
 package wolox.training.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import wolox.training.models.Book;
+import wolox.training.repositories.BookRepository;
+import wolox.training.utils.EndPoints;
 
 
-@Controller
+@RestController
+@RequestMapping(EndPoints.BOOK_BASE_PATH)
 public class BookController {
+
+    @Autowired
+    BookRepository bookRepository;
+
+
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
+    }
+
+    @GetMapping
+    public Iterable findAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Book createBook(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void deleteBook(@PathVariable Long id) {
+        bookRepository.deleteById(id);
     }
 }
---
+++ training/src/main/java/wolox/training/utils/EndPoints.java
@@ -0,0 +1,9 @@
+package wolox.training.utils;
+
+public class EndPoints {
+    public static final String BOOK_BASE_PATH = "/api/books";
+    public static final String BOOKS = "/books";
+
+    private EndPoints() {
+    }
+}
