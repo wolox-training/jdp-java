371ca651274dac33dec91c7751b8650ab96876bc
Juan Daniel <juan.perez@wolox.co>
2021-07-13 09:19:00 -0500

    Javadoc added to the controller, model and repository

--- training/src/main/java/wolox/training/controllers/BookController.java
+++ training/src/main/java/wolox/training/controllers/BookController.java
@@ -20,14 +20,27 @@ import wolox.training.utils.EndPoints;
 
 import java.util.List;
 
+/**
+ * The type Book controller.
+ */
 @RestController
 @RequestMapping(EndPoints.BOOK_BASE_PATH)
 public class BookController {
 
+    /**
+     * The Book repository.
+     */
     @Autowired
     BookRepository bookRepository;
 
 
+    /**
+     * Greetings! with a name
+     *
+     * @param name:  Optional name of who is going to greet (String)
+     * @param model: Contains the data that appears in the view (Model)
+     * @return The name of the view to perform the greeting
+     */
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
                            Model model) {
@@ -35,23 +48,47 @@ public class BookController {
         return "greeting";
     }
 
+    /**
+     * Find all list.
+     *
+     * @return the list
+     */
     @GetMapping
     public List<Book> findAll() {
         return bookRepository.findAll();
     }
 
+    /**
+     * Create book book.
+     *
+     * @param book the book
+     * @return the book
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Book createBook(@RequestBody Book book) {
         return bookRepository.save(book);
     }
 
+    /**
+     * Delete book.
+     *
+     * @param id the id
+     */
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public void deleteBook(@PathVariable Long id) {
         bookRepository.deleteById(id);
     }
 
+    /**
+     * Update book book.
+     *
+     * @param book the book object
+     * @param id   the id
+     * @return the book
+     * @throws IdMismatchException when id param and book's id mismatch
+     */
     @PutMapping("/{id}")
     public Book updateBook(@RequestBody Book book, @PathVariable Long id) {
         if (book.getId() != id) {
--- training/src/main/java/wolox/training/repositories/BookRepository.java
+++ training/src/main/java/wolox/training/repositories/BookRepository.java
@@ -6,8 +6,17 @@ import wolox.training.models.Book;
 
 import java.util.Optional;
 
+/**
+ * The interface Book repository.
+ */
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
+    /**
+     * Find by author optional.
+     *
+     * @param author the author
+     * @return the optional <book>
+     */
     Optional<Book> findByAuthor(String author);
 }
