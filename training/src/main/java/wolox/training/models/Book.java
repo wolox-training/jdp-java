adbd72ee4f5e3969086a8e49d6b86cd4dfddfa17
Juan Daniel <juan.perez@wolox.co>
2021-07-28 09:42:31 -0500

    Getter, NonNull and Constructors annotations added to the models to avoid the boilerplate

--- training/src/main/java/wolox/training/models/Book.java
+++ training/src/main/java/wolox/training/models/Book.java
@@ -2,6 +2,10 @@ package wolox.training.models;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import org.springframework.util.ObjectUtils;
 
 import javax.persistence.Column;
@@ -18,6 +22,9 @@ import static wolox.training.utils.ErrorMessage.*;
 /**
  * The class Book model.
  */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @ApiModel(description = "Books from database")
 public class Book {
@@ -30,52 +37,41 @@ public class Book {
     @ApiModelProperty(notes = "The book genre")
     private String genre;
 
-    @Column(nullable = false)
+    @NonNull
     @ApiModelProperty(notes = "The book's author")
     private String author;
 
-    @Column(nullable = false)
+    @NonNull
     @ApiModelProperty(notes = "The book image")
     private String image;
 
-    @Column(nullable = false)
+    @NonNull
     @ApiModelProperty(notes = "The book title")
     private String title;
 
-    @Column(nullable = false)
+    @NonNull
     @ApiModelProperty(notes = "The book subtitle")
     private String subtitle;
 
-    @Column(nullable = false)
+    @NonNull
     @ApiModelProperty(notes = "The book's publisher")
     private String publisher;
 
-    @Column(nullable = false)
+    @NonNull
     @ApiModelProperty(notes = "The book year of publishing")
     private String year;
 
-    @Column(nullable = false)
+    @NonNull
     @ApiModelProperty(notes = "The number of pages the book has")
     private Integer pages;
 
-    @Column(nullable = false)
+    @NonNull
     @ApiModelProperty(notes = "The code of the book")
     private String isbn;
 
 
-    public Book() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getGenre() {
-        return genre;
     }
 
     public void setGenre(String genre) {
@@ -84,28 +80,17 @@ public class Book {
         this.genre = genre;
     }
 
-    public String getAuthor() {
-        return author;
-    }
-
     public void setAuthor(String author) {
         checkNotNull(author, NOT_NULL_MESSAGE);
         checkArgument(!ObjectUtils.isEmpty(author), EMPTY_MESSAGE);
         this.author = author;
     }
 
-    public String getImage() {
-        return image;
-    }
 
     public void setImage(String image) {
         checkNotNull(image, NOT_NULL_MESSAGE);
         checkArgument(!ObjectUtils.isEmpty(image), EMPTY_MESSAGE);
         this.image = image;
-    }
-
-    public String getTitle() {
-        return title;
     }
 
     public void setTitle(String title) {
@@ -114,28 +99,16 @@ public class Book {
         this.title = title;
     }
 
-    public String getSubtitle() {
-        return subtitle;
-    }
-
     public void setSubtitle(String subtitle) {
         checkNotNull(subtitle, NOT_NULL_MESSAGE);
         checkArgument(!ObjectUtils.isEmpty(subtitle), EMPTY_MESSAGE);
         this.subtitle = subtitle;
     }
 
-    public String getPublisher() {
-        return publisher;
-    }
-
     public void setPublisher(String publisher) {
         checkNotNull(publisher, NOT_NULL_MESSAGE);
         checkArgument(!ObjectUtils.isEmpty(publisher), EMPTY_MESSAGE);
         this.publisher = publisher;
-    }
-
-    public String getYear() {
-        return year;
     }
 
     public void setYear(String year) {
@@ -145,19 +118,11 @@ public class Book {
         this.year = year;
     }
 
-    public Integer getPages() {
-        return pages;
-    }
-
     public void setPages(Integer pages) {
         checkNotNull(pages, NOT_NULL_MESSAGE);
         checkArgument(!ObjectUtils.isEmpty(pages), EMPTY_MESSAGE);
         checkArgument(pages > 0, MORE_PAGES);
         this.pages = pages;
-    }
-
-    public String getIsbn() {
-        return isbn;
     }
 
     public void setIsbn(String isbn) {
--- training/src/main/java/wolox/training/models/User.java
+++ training/src/main/java/wolox/training/models/User.java
@@ -6,10 +6,13 @@ import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import org.springframework.util.ObjectUtils;
 import wolox.training.exceptions.BookAlreadyOwnedException;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -20,13 +23,15 @@ import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static wolox.training.utils.ErrorMessage.*;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "users")
 @ApiModel(description = "Users from database")
@@ -35,25 +40,25 @@ public class User {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    @Column(nullable = false)
+    @NonNull
     @ApiModelProperty(notes = "The user username")
     private String username;
 
-    @Column(nullable = false)
+    @NonNull
     @ApiModelProperty(notes = "The user fullname")
     private String name;
 
-    @Column(nullable = false)
+    @NonNull
     @JsonDeserialize(using = LocalDateDeserializer.class)
     @JsonSerialize(using = LocalDateSerializer.class)
     @ApiModelProperty(notes = "The birth date of the user")
     private LocalDate birthdate;
 
-    @Column(nullable = false)
+    @NonNull
     @ApiModelProperty(notes = "The user password")
     private String password;
 
-    @Column(nullable = false)
+    @NonNull
     @ManyToMany
     @JoinTable(name = "book_user",
             joinColumns = @JoinColumn(name = "book_id", referencedColumnName = "id"),
@@ -62,29 +67,15 @@ public class User {
     private List<Book> books = new ArrayList<>();
 
 
-    public User() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
     public void setId(Long id) {
         this.id = id;
     }
 
-    public String getUsername() {
-        return username;
-    }
 
     public void setUsername(String username) {
         checkNotNull(username, NOT_NULL_MESSAGE);
         checkArgument(!ObjectUtils.isEmpty(username), EMPTY_MESSAGE);
         this.username = username;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public void setName(String name) {
@@ -93,9 +84,6 @@ public class User {
         this.name = name;
     }
 
-    public LocalDate getBirthdate() {
-        return birthdate;
-    }
 
     public void setBirthdate(LocalDate birthdate) {
         checkNotNull(birthdate, NOT_NULL_MESSAGE);
@@ -103,18 +91,12 @@ public class User {
         this.birthdate = birthdate;
     }
 
-    public List<Book> getBooks() {
-        return (List<Book>) Collections.unmodifiableList(books);
-    }
 
     public void setBooks(List<Book> books) {
         checkNotNull(books, NOT_NULL_MESSAGE);
         this.books = books;
     }
 
-    public String getPassword() {
-        return password;
-    }
 
     public void setPassword(String password) {
         this.password = password;
